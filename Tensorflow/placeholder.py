75810f2a6ad119ce71bd9a8e25a2d658118ed026
Chris <tw906022@gmail.com>
2021-04-30 22:25:54 +0800

    2021/04/30

---
+++ Tensorflow/placeholder.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+input1 = tf.compat.v1.placeholder(tf.float32)   # must use (feed_dict) get value
+
+input2 = tf.compat.v1.placeholder(tf.float32)
+
+output = tf.multiply(input1, input2)  # input1 * input2
+
+with tf.compat.v1.Session() as sess:
+    print(sess.run(output, feed_dict={input1:[7],input2:[2]}))
---
+++ Tensorflow/variable.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+
+state = tf.Variable(0, name="counter")
+# print(state.name)
+
+one = tf.constant(1)
+
+new_value = tf.add(state, one)  # state + one
+
+update = tf.compat.v1.assign(state, new_value)  # tf.compat.v1.assign (TensorFlow Core v2.4.1)
+
+init = tf.compat.v1.global_variables_initializer()   # must have if define variable
+
+with tf.compat.v1.Session() as sess:   # use this method you don't need sess.close()
+    sess.run(init)
+    for i in range(6):
+        sess.run(update)
+        print(sess.run(state))  # sess.run()  like a pointer
