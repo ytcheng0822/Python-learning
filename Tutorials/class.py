d58a4686cc1ecd0a80fd395683619844bde84c24
Chris <tw906022@gmail.com>
2022-09-17 22:04:10 +0800

    Fix some spacing

--- Tutorials/class.py
+++ Tutorials/class.py
@@ -1,7 +1,7 @@
 # 定義【類別與類別屬性】(封裝在類別中的變數和函式)
 # 定義一個類別名稱IO, 有兩個類別屬性 supportedSrcs 和 read
 class IO:
-    supportedSrcs=["console","file"]
+    supportedSrcs = ["console", "file"]
     def read(src):
         if (src not in IO.supportedSrcs):
             print("Not Supported")
--- Tutorials/condition.py
+++ Tutorials/condition.py
@@ -13,12 +13,12 @@ n1 = int(input("請輸入數字一: "))
 n2 = int(input("請輸入數字二: "))
 op = input("請輸入運算: +, -, *, / : ")
 if (op == "+"):
-    print(n1+n2)
+    print(n1 + n2)
 elif (op == "-"):
-    print(n1-n2)
+    print(n1 - n2)
 elif (op == "*"):
-    print(n1*n2)
+    print(n1 * n2)
 elif (op == "/"):
-    print(n1/n2)
+    print(n1 / n2)
 else:
     print("不支援的運算")           
